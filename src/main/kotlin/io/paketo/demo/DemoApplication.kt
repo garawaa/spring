41eeb8ba06fba7bd848e3d0ed5db9abcdca3bb68
garawaa <garawaa@gmail.com>
2022-12-11 18:40:04 +0800

    Update DemoApplication.kt

--- src/main/kotlin/io/paketo/demo/DemoApplication.kt
+++ src/main/kotlin/io/paketo/demo/DemoApplication.kt
@@ -9,3 +9,14 @@ class DemoApplication
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
+@RestController
+class MessageResource {
+    @GetMapping("/")
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
+
+data class Message(val id: String?, val text: String)
